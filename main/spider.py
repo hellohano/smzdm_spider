64ddaf13ac27d66470d6c29cfe7c00f5cc894f13
hellohano@gmail.com <whl900528>
2014-07-15 12:02:39 +0800

    get ktxp content

--- main/spider.py
+++ main/spider.py
@@ -1,1 +1,26 @@
 __author__ = 'hano'
+# coding:utf-8
+import urllib2
+
+import BeautifulSoup as bs
+
+
+def main():
+    print getWebContent("http://bt.ktxp.com/today.html#1")
+    # print getWebContent("http://www.baidu.com")
+
+
+def getWebContent(url):
+    if (url == None):
+        return "the url is null !"
+    else:
+        req = urllib2.Request(url)
+        resp = urllib2.urlopen(req)
+        webContent = resp.read()
+        soup = bs.BeautifulSoup(webContent)
+        length = len(soup.findAll('td', attrs={'class': 'ltext ttitle'}))
+        return length
+
+
+if __name__ == '__main__':
+    main()
