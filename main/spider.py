298ac6d311db16176b6d0497f8420d4e89f142e6
hellohano@gmail.com <whl900528>
2014-07-15 16:23:26 +0800

    analyst ktxp item

--- main/spider.py
+++ main/spider.py
@@ -6,7 +6,7 @@ import BeautifulSoup as bs
 
 
 def main():
-    print getWebContent("http://bt.ktxp.com/today.html#1")
+    getWebContent("http://bt.ktxp.com/today.html#1")
     # print getWebContent("http://www.baidu.com")
 
 
@@ -18,8 +18,32 @@ def getWebContent(url):
         resp = urllib2.urlopen(req)
         webContent = resp.read()
         soup = bs.BeautifulSoup(webContent)
-        length = len(soup.findAll('td', attrs={'class': 'ltext ttitle'}))
-        return length
+
+        divs = soup.findAll('div', attrs={'class': 'item-box round-corner space-bottom'})
+
+        for div in divs:
+            # this div contains the section title(like music/game ..) and the list of resource
+            print "========= section ", divs.index(div), div.find('h2').contents[0].string, " begin ========="
+            tbody = div.find('table').find('tbody')
+            resList = tbody.findAll('tr')
+            for item in resList:
+                analystKTXPItem(item)
+            print "========= section ", divs.index(div), " begin ========="
+        return divs
+
+
+def analystKTXPItem(item):
+    # ================== the ktxp res item strut ============================
+    # date|resource title    |resource size|seed nums      |download nums  |publisher
+    # td  |td(ltext ttitle)/a|      td     |td(class/bts-2)|td(class/btl-1)|td/a(class/team-name)
+    # ================== end ================================================
+    date = item.contents[0]
+    title = item.find('td', attrs={'class': 'ltext ttitle'}).contents[1].string
+    size = item.contents[2].string
+    seedNum = item.find('td', attrs={'class': 'bts-2'})
+    DownloadNum = item.find('td', attrs={'class': 'btl-1'})
+    publisher = item.contents[5].find('a', attrs={'class': 'team-name'})
+    print date, '|', title, '|', size, '|', seedNum, '|', DownloadNum, '|', publisher
 
 
 if __name__ == '__main__':
